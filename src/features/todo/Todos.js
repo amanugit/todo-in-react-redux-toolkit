e84e6b167e65952db8e658575f67321d826f6cd9
Amanuel <amannew49@gmail.com>
2023-02-21 12:18:58 +0300

    initial commit

--- src/features/todo/Todos.js
+++ src/features/todo/Todos.js
@@ -8,6 +8,8 @@ export function Todos() {
     <div className={styles.todoContainer}>
       {
         todos.length > 0 ? <p style={{fontStyle: 'italic'}}>Double click on item to toggle completed or not</p> : <p style={{fontStyle: 'italic'}}>Add to do to see more taks</p>
+      }
+      {
         todos.map(todo => (
           <Todo item={todo} key={todo.id}/>
         ))
